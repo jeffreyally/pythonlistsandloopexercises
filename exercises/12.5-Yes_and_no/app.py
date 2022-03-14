5dd862f57192b4ab23734c8814dce0540a7fb8e4
jeffreyally <jeffreyally@gmail.com>
2022-03-14 03:03:33 +0000

    exercise 12.5

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,7 +1,7 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 def wikiwoko(num):
-    str = 'woko' if num == 0 else "wiki" if num == 1 
+    str = 'woko' if num == 0 else "wiki" if num == 1 else None
     return str
 
 print(list(map(wikiwoko,theBools)))
