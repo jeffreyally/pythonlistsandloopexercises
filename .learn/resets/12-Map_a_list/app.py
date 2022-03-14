e0e4cf61b3a46082334bf6d35fa27c1db4346e8f
jeffreyally <jeffreyally@gmail.com>
2022-03-14 02:41:52 +0000

    exercise 12

--- .learn/resets/12-Map_a_list/app.py
+++ .learn/resets/12-Map_a_list/app.py
@@ -3,8 +3,7 @@ Celsius_values = [-2,34,56,-10]
 
 
 def fahrenheit_values(x):
-    return (x*1.8)+32
-
+# the magic go here:
    
 result = list(map(fahrenheit_values, Celsius_values))
 print(result)
