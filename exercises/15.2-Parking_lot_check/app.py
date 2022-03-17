bee96c8853be11ad81f0ca3d5b1a002ec6ae35c0
jeffreyally <jeffreyally@gmail.com>
2022-03-17 03:54:32 +0000

    15.2 parking lot

--- exercises/15.2-Parking_lot_check/app.py
+++ exercises/15.2-Parking_lot_check/app.py
@@ -1,9 +1,41 @@
 parking_state = [
-  [1,1,1],
-  [0,0,0],
-  [1,1,2]
+  [1,0,1,1,0,1],
+  [2,0,1,1,0,1],
+  [1,0,2,1,0,1],
+  [1,0,1,1,0,1],
+  [1,0,1,1,0,2],
+  [1,0,1,1,0,1],
 ]
 
-#Your code go here:
+def get_parking_lot(parking_state):
+  
 
 
+  state1 = {
+    "total_slots":0,
+    "available_slots":0,
+    "occupied_slots":0
+  }
+  for lists in parking_state:
+    for elements in lists:
+      if elements == 0:
+        continue
+      
+      state1['total_slots'] +=1
+      if 'occupied_slots' not in state1:
+        if elements == 1:
+          state1['occupied_slots'] = 1
+          continue
+          
+      
+          
+      if elements == 1:
+          state1['occupied_slots'] += 1
+    
+      if elements == 2:
+         state1['available_slots'] += 1
+  return state1
+    
+    
+
+print(get_parking_lot(parking_state))
