89548456e22b8d78c4af7b334314a7f79fd616b4
jeffreyally <jeffreyally@gmail.com>
2022-03-17 17:22:13 +0000

    techno beats 16

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,3 +1,20 @@
+import re
+
+def lyrics_generator(listOfNumbers):
+    Regex = re.compile(r'Drop the base Drop the base Drop the base ')
+    
+
+    lyrics = ''
+    for numbers in listOfNumbers:
+        if numbers == 0:
+            lyrics = lyrics + 'Boom '
+        if numbers == 1:
+            lyrics = lyrics + 'Drop the base '
+    
+    mo = Regex.sub('Drop the base Drop the base Drop the base !!!Break the base!!! ',lyrics)
+    
+    return mo
+
 
 
 # Your code go above, nothing to change after this line:
@@ -6,3 +23,9 @@ print(lyrics_generator([0,0,1,1,1,0,0,0]))
 print(lyrics_generator([0,0,0]))
 print(lyrics_generator([1,0,1]))
 print(lyrics_generator([1,1,1]))
+
+# Regex = re.compile(r'Drop the base Drop the base Drop the base ')
+# mo = Regex.sub('Drop the base Drop the base Drop the base !!!Break the base!!!',lyrics_generator([1,0,1]))
+
+# print(mo)
+
