11ec2cf75df3770a2b206760c73ea82ab1d052dc
jeffreyally <jeffreyally@gmail.com>
2022-03-17 03:01:21 +0000

    15.1

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,17 @@
 #Import random
 
 #Create the function below:
+
+
+def matrixBuilder(num):
+    matrixlist = []
+    for i in range(num):
+        matrixlist.append([])
+    for i in range(num):
+        for j in range(num):
+            matrixlist[j].append(1)
+    return matrixlist
+    
+
+print(matrixBuilder(3))
+
