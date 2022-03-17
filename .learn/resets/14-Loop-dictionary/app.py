f955c092675562fc3efb9b68a634484bff5c09c3
jeffreyally <jeffreyally@gmail.com>
2022-03-17 02:30:27 +0000

    14.2 letter count

--- .learn/resets/14-Loop-dictionary/app.py
+++ .learn/resets/14-Loop-dictionary/app.py
@@ -1,8 +1,5 @@
 spanish_translations = { "dog": "perro", "house": "casa", "cat": "gato" }
-
-spanish_translations['love'] = 'amor'
-spanish_translations['code']= 'codigo'
-spanish_translations['smart'] = 'inteligente'
+# Your code here
 
 
 # Don't touch the code below
--- .learn/resets/14.2-letter_counter/app.py
+++ .learn/resets/14.2-letter_counter/app.py
@@ -1,13 +1,7 @@
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 
 counts = {}
-for letter in par:
-    if letter == ' ':
-        continue
-    if letter not in counts:
-        counts[letter.lower()] = 1
-    else:
-        counts[letter.lower()] += 1
+#your code go here:
 
 print(counts)
 
